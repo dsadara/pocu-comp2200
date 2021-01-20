1ba147f5bfda9d742a50aa5cfc4f651ce3584b44
DESKTOP-E10Q9V0\lim97 <lim971230@yonsei.ac.kr>
2021-01-20 12:16:17 +0900

    Fix formatting

--- Assignment1/pomoku.c
+++ Assignment1/pomoku.c
@@ -6,14 +6,14 @@ size_t g_player0_score = 0;
 size_t g_player1_score = 0;
 int g_board[20][20];
 
-size_t s_horizontal_score_before_black;
-size_t s_horizontal_score_before_white;
-size_t s_vertical_score_before_black;
-size_t s_vertical_score_before_white;
-size_t s_left_diagonal_score_before_black;
-size_t s_left_diagonal_score_before_white;
-size_t s_right_diagonal_score_before_black;
-size_t s_right_diagonal_score_before_white;
+size_t g_horizontal_score_before_black;
+size_t g_horizontal_score_before_white;
+size_t g_vertical_score_before_black;
+size_t g_vertical_score_before_white;
+size_t g_left_diagonal_score_before_black;
+size_t g_left_diagonal_score_before_white;
+size_t g_right_diagonal_score_before_black;
+size_t g_right_diagonal_score_before_white;
 
 void init_game(void)
 {
@@ -25,10 +25,10 @@ void init_game(void)
     g_player0_score = 0;
     g_player1_score = 0;
 
-    s_horizontal_score_before_white = 0;
-    s_horizontal_score_before_black = 0;
-    s_vertical_score_before_black = 0;
-    s_vertical_score_before_white = 0;
+    g_horizontal_score_before_white = 0;
+    g_horizontal_score_before_black = 0;
+    g_vertical_score_before_black = 0;
+    g_vertical_score_before_white = 0;
 }
 
 size_t get_row_count(void)
@@ -81,62 +81,62 @@ int place_stone(const color_t color, const size_t row, const size_t col)
     size_t left_diagonal_score_temp;
     size_t right_diagonal_score_temp;
 
-    if(!is_placeable(row, col)) {
+    if (!is_placeable(row, col)) {
         return FALSE;
     }
 
     g_board[row][col] = color;
 
-    if(color == COLOR_BLACK) {
+    if (color == COLOR_BLACK) {
 
         horizontal_score_temp = count_horizontal_chain_score(COLOR_BLACK, row);
-        if (horizontal_score_temp != s_horizontal_score_before_black) {
-            s_horizontal_score_before_black = horizontal_score_temp;
-            g_player0_score += s_horizontal_score_before_black;
+        if (horizontal_score_temp != g_horizontal_score_before_black) {
+            g_horizontal_score_before_black = horizontal_score_temp;
+            g_player0_score += g_horizontal_score_before_black;
         }
 
         vertical_score_temp = count_vertical_chain_score(COLOR_BLACK, col);
-        if (vertical_score_temp != s_vertical_score_before_black) {
-            s_vertical_score_before_black = vertical_score_temp;
-            g_player0_score += s_vertical_score_before_black;
+        if (vertical_score_temp != g_vertical_score_before_black) {
+            g_vertical_score_before_black = vertical_score_temp;
+            g_player0_score += g_vertical_score_before_black;
         }
 
         left_diagonal_score_temp = count_left_diagonal_chain_score(COLOR_BLACK, row, col);
-        if (left_diagonal_score_temp != s_left_diagonal_score_before_black) {
-            s_left_diagonal_score_before_black = left_diagonal_score_temp;
-            g_player0_score += s_left_diagonal_score_before_black;
+        if (left_diagonal_score_temp != g_left_diagonal_score_before_black) {
+            g_left_diagonal_score_before_black = left_diagonal_score_temp;
+            g_player0_score += g_left_diagonal_score_before_black;
         }
 
         right_diagonal_score_temp = count_right_diagonal_chain_score(COLOR_BLACK, row, col);
-        if (right_diagonal_score_temp != s_right_diagonal_score_before_black) {
-            s_right_diagonal_score_before_black = right_diagonal_score_temp;
-            g_player0_score += s_right_diagonal_score_before_black;
+        if (right_diagonal_score_temp != g_right_diagonal_score_before_black) {
+            g_right_diagonal_score_before_black = right_diagonal_score_temp;
+            g_player0_score += g_right_diagonal_score_before_black;
         }
 
-    } else if (color == COLOR_WHITE){
+    } else if (color == COLOR_WHITE) {
 
         horizontal_score_temp = count_horizontal_chain_score(COLOR_WHITE, row);
-        if (horizontal_score_temp != s_horizontal_score_before_white) {
-            s_horizontal_score_before_white = horizontal_score_temp;
-            g_player1_score += s_horizontal_score_before_white;
+        if (horizontal_score_temp != g_horizontal_score_before_white) {
+            g_horizontal_score_before_white = horizontal_score_temp;
+            g_player1_score += g_horizontal_score_before_white;
         }
 
         vertical_score_temp = count_vertical_chain_score(COLOR_WHITE, col);
-        if (vertical_score_temp != s_vertical_score_before_white) {
-            s_vertical_score_before_white = vertical_score_temp;
-            g_player1_score += s_vertical_score_before_white;
+        if (vertical_score_temp != g_vertical_score_before_white) {
+            g_vertical_score_before_white = vertical_score_temp;
+            g_player1_score += g_vertical_score_before_white;
         }
 
         left_diagonal_score_temp = count_left_diagonal_chain_score(COLOR_WHITE, row, col);
-        if (left_diagonal_score_temp != s_left_diagonal_score_before_white) {
-            s_left_diagonal_score_before_white = left_diagonal_score_temp;
-            g_player1_score += s_left_diagonal_score_before_white;
+        if (left_diagonal_score_temp != g_left_diagonal_score_before_white) {
+            g_left_diagonal_score_before_white = left_diagonal_score_temp;
+            g_player1_score += g_left_diagonal_score_before_white;
         }
 
         right_diagonal_score_temp = count_right_diagonal_chain_score(COLOR_WHITE, row, col);
-        if (right_diagonal_score_temp != s_right_diagonal_score_before_white) {
-            s_right_diagonal_score_before_white = right_diagonal_score_temp;
-            g_player1_score += s_right_diagonal_score_before_white;
+        if (right_diagonal_score_temp != g_right_diagonal_score_before_white) {
+            g_right_diagonal_score_before_white = right_diagonal_score_temp;
+            g_player1_score += g_right_diagonal_score_before_white;
         }
     }
     return TRUE;
@@ -160,11 +160,11 @@ size_t count_horizontal_chain_score(const color_t color, const size_t row)
     size_t score_sum = 0;
     size_t stone_sum = 0;
 
-    for(i = 0; i < g_board_width; ++i) {
-        if(g_board[row][i] == color) {
+    for (i = 0; i < g_board_width; ++i) {
+        if (g_board[row][i] == color) {
             stone_sum++;
         } else {
-            if(stone_sum >= 5) {
+            if (stone_sum >= 5) {
                 score_sum += get_score_per_stone(stone_sum);
             }
             stone_sum = 0;
@@ -181,7 +181,7 @@ size_t count_vertical_chain_score(const color_t color, const size_t col)
     size_t score_sum = 0;
     size_t stone_sum = 0;
 
-    for(i = 0; i < g_board_height; ++i) {
+    for (i = 0; i < g_board_height; ++i) {
         if (g_board[i][col] == color) {
             stone_sum++;
         } else {
@@ -197,7 +197,7 @@ size_t count_vertical_chain_score(const color_t color, const size_t col)
 
 size_t get_score_per_stone(size_t stone_sum)
 {
-    if(stone_sum < 5 || stone_sum > 20) {
+    if (stone_sum < 5 || stone_sum > 20) {
         return 0;
     }
 
@@ -218,7 +218,7 @@ size_t count_left_diagonal_chain_score(const color_t color, const size_t row, co
     int downright = TRUE;
     int upleft = TRUE;
 
-    for(i = 1; i <= min_diagonal_length; i++) {
+    for (i = 1; i <= min_diagonal_length; i++) {
         row_downright = row + i;
         col_downright = col + i;
         row_upleft = row - i;
@@ -262,7 +262,7 @@ size_t count_right_diagonal_chain_score(const color_t color, const size_t row, c
     int downleft = TRUE;
     int upright = TRUE;
 
-    for(i = 1; i <= min_diagonal_length; i++) {
+    for (i = 1; i <= min_diagonal_length; i++) {
         row_downleft = row + i;
         col_downleft = col - i;
         row_upright = row - i;
