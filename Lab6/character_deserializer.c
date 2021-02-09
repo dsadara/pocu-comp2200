310d0682d6221113c96749bf3f9275439ff9ca9c
DESKTOP-I4VE631\채현 <lim971230@yonsei.ac.kr>
2021-02-09 22:43:33 +0900

    함수 미구현 코드 업로드

---
+++ Lab6/character_deserializer.c
@@ -0,0 +1,108 @@
+#include "character_deserializer.h"
+
+int get_character(const char* filename, character_v3_t* out_character)
+{
+    FILE* stream;
+    char character;
+    char delims_count = 0;
+    int version = -1;
+
+    stream = fopen(filename, "r");
+
+    while ((character = fgetc(stream)) != EOF) {
+        if (character == ',') {
+            delims_count++;
+        }
+    }
+
+    if (delims_count == 7) {
+        printf("delims_count: %d\n", delims_count);
+        version = 1;
+    }
+
+    if (delims_count == 18) {
+        printf("delims_count: %d\n", delims_count);
+        version = 2;
+    }
+
+    if (delims_count == 0) {
+        printf("delims_count: %d\n", delims_count);
+        deserialize_v3(stream, out_character);
+        version = 3;
+    }
+    fclose(stream);
+
+    return version;
+}
+/*
+void deserialize_v1_to_v3(FILE* stream, character_v3_t* out_character)
+{
+
+}
+
+void deserialize_v2_to_v3(FILE* stream, character_v3_t* out_character)
+{
+
+}
+*/
+void deserialize_v3(FILE* stream, character_v3_t* out_character)
+{
+    char buffer[4096];
+    char tmp;
+    int result;
+    size_t minion_count;
+    size_t i;
+    size_t line_length;
+    
+    fseek(stream, 0, SEEK_SET); /* 테스트중에 스트림 초기화가 안되서 사용중 */
+    line_length = get_line_length(stream);
+    printf("line length: %d\n", line_length);
+    result = fseek(stream, (line_length + 2) * sizeof(char), SEEK_SET);
+    if (result != 0) {
+        perror("error while seeking");
+        return;
+    }
+    
+    fgets(buffer, line_length, stream);
+    remove_column(buffer);
+    sscanf(buffer, "%s%d%d%d%d%d%d%d%d%d%d%d%d%zd",
+    out_character->name, &out_character->level, &out_character->health,
+    &out_character->mana, &out_character->strength, &out_character->dexterity,
+    &out_character->intelligence, &out_character->armour, &out_character->evasion,
+    &out_character->elemental_resistance.fire, &out_character->elemental_resistance.cold, &out_character->elemental_resistance.lightning,
+    &out_character->leadership, &out_character->minion_count);
+
+    minion_count = out_character->minion_count;
+    if (minion_count == 0) {
+        return;
+    }
+    printf("next char: %c\n", fgetc(stream));
+
+    /*
+    for (i = 0; i < minion_count; i++) {
+        
+    }
+    */
+}
+
+void remove_column(char* buffer)
+{
+    char* character = buffer;
+
+    while(*character++ != '\0') {
+        if (*character == '|') {
+            *character = ' ';
+        }
+    }
+}
+
+size_t get_line_length(FILE* stream)
+{
+    size_t length_count = 0;
+
+    while (fgetc(stream) != '\n') {
+        length_count++;
+    }
+
+    return length_count;
+}
