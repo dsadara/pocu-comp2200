3de1c5081a617d466db4322cb0117eabddcf7085
DESKTOP-I4VE631\채현 <lim971230@yonsei.ac.kr>
2021-03-19 22:50:38 +0900

    매크로 함수 인자로 표현식이 들어올 경우

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -1,20 +1,20 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define MAX(x, y) ((x > y) ? x : y)
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
-#define MIN(x, y) ((x > y) ? y : x)
+#define MIN(x, y) (((x) > (y)) ? (y) : (x))
 
-#define ABS(x) ((x >= 0) ? x : -x)
+#define ABS(x) (((x) >= 0) ? (x) : -(x))
 
-#define RANGE(curr, min, max) for (curr = min; curr <= max; curr++)
+#define RANGE(curr, min, max) for ((curr) = (min); (curr) <= (max); (curr)++)
 
-#define RANGE_DESC(curr, max, min) for (curr = max; curr >= min; curr--)
+#define RANGE_DESC(curr, max, min) for ((curr) = (max); (curr) >= (min); (curr)--)
 
 #define SET(ary, start, count, value) {                  \
     size_t i;                                            \
-    for (i = 0; i < count; i++) {                        \
-        ary[start + i] = value;                          \
+    for (i = 0; i < (count); i++) {                        \
+        ary[(start) + i] = (value);                          \
     }                                                    \
 }                                                        \
 
